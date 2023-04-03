e9b18628b8799eae0893a7fd898f3c0080691ff8
Abyan_PB <abyanpb@gmail.com>
2023-04-03 10:47:16 +0700

    update

---
+++ Sistem-Monitoring/database/migrations/2023_04_03_033515_create_kebersihan-ruangan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kebersihan-ruangan', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nama Ruangan');
+            $table->enum('level', array('Bersih', 'Kotor', 'Sedang dikerjakan'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
